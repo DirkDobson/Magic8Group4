758d3be098eac2c653a07dcf4ff3d7c6cf811932
Dirk Dobson <dirkvancouver@live.com>
2018-08-16 15:02:15 -0600

    created menu

--- main.rb
+++ main.rb
@@ -1,2 +1,23 @@
-num = rand(8)
-puts num
+#create greeting menu
+#If else that gets.to_s if QUIT exit
+#If not QUIT 'else' rand (1..9 +(other_answers_generated))
+require ' answers.rb'
+
+@answers = []
+
+class MainMenu
+end
+
+def initialize(name)
+  @name = name
+end
+
+def menu
+  puts "Welcome to Magic8Group4"
+  puts "Ask a Question"
+  question = gets.to_s.strip.downcase
+  if question == quit
+    exit
+  else rolled_number
+end
+
