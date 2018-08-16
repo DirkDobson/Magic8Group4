d91a66a1b9b3fe4c123be5d77a7e6fbf04b4f622
Brianne Duffy <duffymbrianne@gmail.com>
2018-08-16 14:55:13 -0600

    add 4 answers

---
+++ answers.rb
@@ -0,0 +1,13 @@
+require 'main.rb'
+
+def rolled_number
+  case number
+  when 1
+    puts "Roll again."
+  when 2
+    puts "Your dreams will come true."
+  when 3
+    puts "I'm not so sure."
+  when 4
+    puts "Most definitely."
+end
